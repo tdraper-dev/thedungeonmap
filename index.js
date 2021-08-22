24ce89ca5e3a4e079be8412e1c8be8b7ed060769
Draper-dev <tdanieldraper@gmail.com>
2021-08-21 23:50:59 -0400

    .

--- index.js
+++ index.js
@@ -42,7 +42,18 @@ app.use(cors())
 
 app.use(express.json())
 app.use(mongoSanitize())
-app.use(helmet())
+app.use(  helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    connectSrc: ["'self'"],
+    frameSrc: ["'self'"],
+    // childSrc: ["'self'", 'https://js.stripe.com'],
+    scriptSrc: ["'self'"],
+    styleSrc: ["'self'"],
+    fontSrc: ["'self'"],
+    imgSrc: ["'self'"],
+  }
+})
 
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
